fd97b8139d4bfd27fb0a733ce81f9312c21bee53
TzviHouminer <tzvi.houminer@gmail.com>
2023-03-06 14:13:06 +0200

    05.03

---
+++ advanced.js/05.03/mission_bg_change/js/bg.js
@@ -0,0 +1,22 @@
+colors = {
+    1:'#1e81b0'
+    2:'#eeeee4'
+    3:'#e28743'
+    4:'#eab676'
+    5:'#76b5c5'
+    6:'#21130d'
+    7:'#873e23'
+    8:'#abdbe3'
+    9:'#063970'
+    10:'#154c79'
+};
+
+setInterval(() => {
+    
+}, 1000);
+
+const bgColor = () => {
+    
+};
+
+document.getElementById("bodyID")
