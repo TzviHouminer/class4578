1cf664e1c650d125ed514e9e8e9c983d07abaf01
TzviHouminer <tzvi.houminer@gmail.com>
2023-03-06 15:05:36 +0200

    ending project

--- project1/js/project_js.js
+++ project1/js/project_js.js
@@ -19,16 +19,16 @@ const addMission = () => {
 
 const printMissions = () => {
     var result = '';
-    missions.map((mission) => {
+    for(let index=missions.length-1; index>=0; index--){
         result += `
         <div class="note d-flex flex-column px-3 pt-4 pb-2">
-            <i class="bi bi-x-square-fill align-self-end mx-3 removeButton" onclick="eraseNote(${mission.id})"></i>
-            <div class="noteContent">${mission.content}</div>
-            <div class="align-self-start mt-auto">${mission.date}</div>
-            <div class="justify-content-end align-self-start">${mission.time}</div>
+            <i class="bi bi-x-square-fill align-self-end mx-3 removeButton" onclick="eraseNote(${missions[index].id})"></i>
+            <div class="noteContent">${missions[index].content}</div>
+            <div class="align-self-start mt-auto">${missions[index].date}</div>
+            <div class="justify-content-end align-self-start">${missions[index].time}</div>
         </div>
         `
-    });
+    };
     document.getElementById("notesArea").innerHTML = result;
 };
 
