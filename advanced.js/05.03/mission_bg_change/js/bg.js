ac65623e3d2107a318edab238b1801cf1cb96b7d
TzviHouminer <tzvi.houminer@gmail.com>
2023-03-10 15:37:39 +0200

    mission done bg color

--- advanced.js/05.03/mission_bg_change/js/bg.js
+++ advanced.js/05.03/mission_bg_change/js/bg.js
@@ -1,22 +1,22 @@
-colors = {
-    1:'#1e81b0'
-    2:'#eeeee4'
-    3:'#e28743'
-    4:'#eab676'
-    5:'#76b5c5'
-    6:'#21130d'
-    7:'#873e23'
-    8:'#abdbe3'
+const colors = {
+    0:'#1e81b0',
+    1:'#154c79',
+    2:'#eeeee4',
+    3:'#e28743',
+    4:'#eab676',
+    5:'#76b5c5',
+    6:'#21130d',
+    7:'#873e23',
+    8:'#abdbe3',
     9:'#063970'
-    10:'#154c79'
 };
-
-setInterval(() => {
-    
-}, 1000);
-
-const bgColor = () => {
-    
+// setInterval(() => {
+//     document.body.style.backgroundColor = colors[Math.floor(Math.random(10)*10)];
+// }, 1000);
+const speedChange = () => {
+    let speed = document.getElementById("slider").value;
+    setInterval(() => {
+        document.body.style.backgroundColor = colors[Math.floor(Math.random(10)*10)]
+    }, speed);
 };
-
-document.getElementById("bodyID")
+speedChange();
